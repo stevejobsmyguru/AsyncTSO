2d8fd0826930be95c5a943bcb8884144dd9fef44
stevejobsmyguru <hariharansankara@gmail.com>
2019-08-23 19:04:52 +0530

    Add files via upload

---
+++ ML_Algo_TestArea/To-be-Sourced-for-debugging_1.R
@@ -0,0 +1,28 @@
+#Pls. change the location of these files to your folder location
+# You need to install KFKSDS from R Stutio -> Tools -> Install Packages. It install C Library of KFKSDS
+
+#Note: do not install TSOutliers from Internet R Repository. They supressed STSM concepts in recent version 
+
+source('<Local Folder Location>/KF.R')
+source('<Local Folder Location>/KF-interfaces.R')
+source('<Local Folder Location>/KFKSDS.R')
+source('<Local Folder Location>/stsm-class.R')
+source('<Local Folder Location>/stsm-methods.R')
+source('<Local Folder Location>/stsmFit.R')
+source('<Local Folder Location>/transPars.R')
+source('<Local Folder Location>/locate-outliers.R')
+source('<Local Folder Location>/frequency-domain.R')
+source('<Local Folder Location>/maxlik-td-optim.R')
+source('<Local Folder Location>/discard-outliers.R')
+source('<Local Folder Location>/mloglik-td.R')
+source('<Local Folder Location>/InfluxTSOutliers.R')
+source('<Local Folder Location>/ProcessData.R')
+source('<Local Folder Location>/BotEntryMethods.R')
+source('<Local Folder Location>/tso.R')
+source('<Local Folder Location>/outliers-tstatistics.R')
+source('<Local Folder Location>/find-consecutive-outliers.R')
+source('<Local Folder Location>/outliers-regressors-stsm.R')
+source('<Local Folder Location>/outliers-regressors-arima.R')
+source('<Local Folder Location>/outliers-data-frame.R')
+source('<Local Folder Location>/outliers-effects.R')
+source('<Local Folder Location>/outliers-tstatistics.R')
